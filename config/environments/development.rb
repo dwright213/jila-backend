4de469094140acb4d4daf0a79b7ac0dd58a52e2b
Dan Wright <muscle.crab.13@gmail.com>
2016-01-17 20:14:00 -0800

    fix typo in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  backend_url = 'http:/localhost:3000'
+  backend_url = 'http://localhost:3000'
 
   config.paperclip_defaults = {
     :storage => :filesystem,
